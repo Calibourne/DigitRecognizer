6850c8b7da0e9d2f3a4f929f3af02d9679314e68
Calibourne <Eddiekanevsky@gmail.com>
2022-06-02 23:35:11 +0300

    progressed with loading data

--- program.py
+++ program.py
@@ -3,22 +3,30 @@ from perceptron import Perceptron
 import utils
 
 def load_data():
-    ts_im_f = open("Data/testimages", "r")
-    ts_lb_f = open("Data/testlabels", "r")
-    
     tr_im_f = open("Data/trainingimages", "r")
     tr_lb_f = open("Data/traininglabels", "r")
     
     v_im_f = open("Data/validationimages", "r")
     v_lb_f = open("Data/validationlabels", "r")
     
-    ts_im_c, ts_lb_c = ts_im_f.readlines(), ts_lb_f.readlines()
+    ts_im_f = open("Data/testimages", "r")
+    ts_lb_f = open("Data/testlabels", "r")
+    
     tr_im_c, tr_lb_c = tr_im_f.readlines(), tr_lb_f.readlines()
     v_im_c, v_lb_c = v_im_f.readlines(), v_lb_f.readlines()
+    ts_im_c, ts_lb_c = ts_im_f.readlines(), ts_lb_f.readlines()
     
-    ts_im_c = utils.join_K_rows(utils.convert_to_bits(ts_im_c))
+    tr_lb_c = list(map(int,utils.remove_nl(tr_lb_c))) 
+    v_lb_c = list(map(int,utils.remove_nl(v_lb_c)))
+    ts_lb_c = list(map(int,utils.remove_nl(ts_lb_c)))
+    
     tr_im_c = utils.join_K_rows(utils.convert_to_bits(tr_im_c))
     v_im_c = utils.join_K_rows(utils.convert_to_bits(v_im_c))
+    ts_im_c = utils.join_K_rows(utils.convert_to_bits(ts_im_c))
+    
+    tr_pairs = utils.pair_label2features(tr_lb_c, tr_im_c)
+    v_pairs = utils.pair_label2features(v_lb_c, v_im_c)
+    ts_pairs = utils.pair_label2features(ts_lb_c, ts_im_c)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 import constants
+def remove_nl(lst: list):
+    removed = [
+        r.replace("\n","") 
+        for r in lst
+    ]
+    return removed
+    
 def convert_to_bits(lst: list):
+    lst = remove_nl(lst)
     converted = [
-        r.replace("\n","").replace(" ", "0").replace("+", "#").replace("#","1") 
+        r.replace(" ", "0").replace("+", "#").replace("#","1") 
         for r in lst
     ]
     return converted
@@ -9,11 +17,20 @@ def convert_to_bits(lst: list):
 def join_K_rows(lst: list, K = constants.ROWS_COUNT):
     K_lines = range(0,len(lst), K)
     joined = []
-    for line, num in enumerate(K_lines):
-        if line in K_lines:
+    for num in range(0,len(lst), K):
+        if num in K_lines:
             joined.append(
                 list(map(int,
                     list(''.join(lst[num:num+constants.ROWS_COUNT]))
                 ))
             )
     return joined
+
+def pair_label2features(lbls: list, features: list):
+        if len(lbls) != len(features):
+            raise Exception(f"Lists do not match in length! lbls len: {len(lbls)} feats len: {len(features)}")
+        paired = [
+            (lbls[i], features[i])
+            for i in range(len(lbls))
+        ]
+        return paired
